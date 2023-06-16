775dfdeea71fe2ddc1c7c5c9eaf7b76e80202b21
주소미 <wnthal1211@gmail.com>
2023-06-16 08:14:51 +0000

    작업형3

--- hypothesis_test.py
+++ hypothesis_test.py
@@ -154,3 +154,75 @@ print(cv_mean_diff)
 if tstat_mean_diff.statistic < cv_mean_diff:
 	print("H0 is rejected under 0.05")
 else: print("H0 is NOT rejected under 0.05")
+    
+    
+    
+###################카이제곱검정###############################
+# 범주형 자료의 독립성 검정에 이용
+#Contingency Table(분할표)는 범주형 자료들의 표본으로 재구성한 표
+#범주 간의 독립 여부는 행의 확률과 열의 확률의 곱이 전체 표본에서
+#분할표에 해당하는 값의 비율과 얼마나 차이 나는지로 검정 
+#Contingency Table -> pandas.crosstab
+#scipy.stats.chi2_contingency
+
+import pandas as pd
+
+a = pd.read_csv('data/blood_pressure.csv', index_col=0)
+
+
+# 사용자 코딩
+#귀무가설: independence
+#대립가설: dependent
+import scipy
+from scipy import stats
+
+#1. Contingency Table 
+cont_table = pd.crosstab(a['sex'],a['agegrp'])
+print(cont_table)
+
+
+#2. Chi*2 test
+from scipy.stats import chi2_contingency
+#return 값은 첫번째가 statistic, 2. p-value , 3. df
+chi2_test = chi2_contingency(cont_table)
+print(chi2_test)
+print("Test Statistic: ", chi2_test[0])
+print("pvalue: ", chi2_test[1])
+# Test Statistic:  0.0
+# pvalue:  1.0
+
+#Test
+alpha = 0.05
+if chi2_test[1] < alpha:
+		print("H0 is rejected under 0.05: Two variables are dependent") 
+		#귀무가설이 기각되면 의존적이라는 것을 의미
+	else:
+		print("H0 is NOT rejected under 0.05 : Two variables are independent")
+
+
+
+        
+##################윌콕슨 부호 검정#######################
+#윌콕슨 검정은 두 집단의 분포에 대한 유사도를 검정하는 기법
+#wilcoxon rank-sum => scipy.stats.ranksums
+#wilcoxon signed-rank => scipy.stats.wilcoxon
+
+#paired sample이 같은 분포로부터 도출(귀무가설)
+#서로 다른 분포로부터 도출되었다. (대립가설)
+
+#Non-parametric paired T-test라면
+#H0 - same distribution
+import scipy
+from scipy import stats
+from scipy.stats import wilcoxon
+
+wilcoxon = scipy.stats.wilcoxon(a['bp_after'], a['bp_before'], alternative ='less')
+print("Test statistic:", wilcoxon.statistic.round(4))
+print("p-value:", wilcoxon.pvalue.round(4))
+#> WilcoxonResult(statistic=2234.5, pvalue=0.0007053666782721429)
+
+if wilcoxon.pvalue<0.05:
+	print("H0 is rejected under 0.05: different distribution.")
+else:
+	print("H0 is NOT rejected under 0.05: same distribution.")
+#귀무가설 기각 -> 치료전 분포와 치료 후 분포는 다른 분포로부터 도출되었다. 
